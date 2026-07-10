de0300d9c7b5166bfd49b355c5be4d66c32bc3fe
muzaif <muzaif31@gmai.com>
2026-07-10 14:11:39 +0530

    changes for mobile

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -12,30 +12,30 @@ import { CgMail } from "react-icons/cg";
 
 function home() {
   return (
-    <div className="h-[100vh]">
+    <div className="h-[90vh] lg:h-[100vh]">
       
-      <div className="h-full flex flex-col justify-evenly ">
+      <div className="h-full flex flex-col justify-evenly lg:h-full">
       <div className="flex px-5">
         <div className="">
         <p className="text-[32px] font-bold m-0">Hi ,I'm</p>
-        <p className="font-bold text-[44px] m-0">MUZAIF SHAIKH</p>
-        <p className="font-bold text-4xl text-purple-700">
+        <p className="font-bold text-[38px] m-0 lg:text-[44px]">MUZAIF SHAIKH</p>
+        <p className="font-bold text-3xl text-purple-700 lg:text-4xl">
           Full Stack Developer
         </p>
-        <p className="w-[80%] text-2xl mt-4 font-semibold ">
+        <p className="text-justify text-2xl mt-4 font-semibold lg:w-[80%]">
           I build exceptional and responsive web applications that provide great
           user experiences.
         </p>
         <div className="flex gap-2 mt-9">
           <a
             href="#project"
-            className="flex text-lg font-semibold border-1 p-2 rounded gap-2 text-white bg-purple-700 items-center"
+            className="flex text-sm font-semibold border-1 px-2 rounded gap-2 text-white bg-purple-700 items-center lg:text-lg lg:p-2"
           >
             View my work <MoveRight />
           </a>
           <a
             href={resume} target="_blank"
-            className="flex text-lg font-semibold border-1 p-2 rounded gap-2 items-center"
+            className="flex text-sm font-semibold border-1 p-2 rounded gap-2 items-center lg:text-lg lg:p-2"
           >
             Download Resume <Download />
           </a>
--- src/pages/skills.jsx
+++ src/pages/skills.jsx
@@ -8,43 +8,43 @@ function Skills(){
             <h2 className="font-bold text-3xl">Skills</h2>
             </div>
             <div className="flex flex-wrap gap-4 justify-center">
-           <div className="h-40 w-40 flex flex-col gap-2">
+           <div className="h-35 w-35 flex flex-col gap-2 lg:h-40 lg:w-40">
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcR5U16C8yXgBpl7-Bc7Itjx3_LRl425zINA&s" alt="react img" className="h-30 object-contain p-3 rounded-lg border-gray-50 shadow-2xl"/>
             <p className="text-center font-bold text-lg">React JS</p>
            </div>
-           <div className="h-40 w-40 flex flex-col gap-2">
+           <div className="h-35 w-35 flex flex-col gap-2 lg:h-40 lg:w-40">
             <img src="https://images.icon-icons.com/2415/PNG/512/nodejs_original_logo_icon_146411.png" alt="react img" className="h-30 object-contain p-3 rounded-lg border-gray-50 shadow-2xl"/>
             <p className="text-center font-bold text-lg">Node JS</p>
            </div>
-           <div className="h-40 w-40 flex flex-col gap-2">
+           <div className="h-35 w-35 flex flex-col gap-2 lg:h-40 lg:w-40">
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUlDx-wmb9kv0mAr6Ff4eG1y9dwUCJ9sYXPQ&s" alt="react img" className="h-30 object-contain p-3 rounded-lg border-gray-50 shadow-2xl"/>
             <p className="text-center font-bold text-lg">Express JS</p>
            </div>
-           <div className="h-40 w-40 flex flex-col gap-2">
+           <div className="h-35 w-35 flex flex-col gap-2 lg:h-40 lg:w-40">
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/1280px-Postgresql_elephant.svg.png" alt="react img" className="h-30 object-contain p-3 rounded-lg border-gray-50 shadow-2xl"/>
             <p className="text-center font-bold text-lg">PostgreSQL</p>
            </div>
-           <div className="h-40 w-40 flex flex-col gap-2">
+           <div className="h-35 w-35 flex flex-col gap-2 lg:h-40 lg:w-40">
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/250px-Unofficial_JavaScript_logo_2.svg.png?utm_source=commons.wikimedia.org&utm_campaign=parser&utm_content=thumbnail" alt="react img" className="h-30 object-contain p-3 rounded-lg border-gray-50 shadow-2xl"/>
             <p className="text-center font-bold text-lg">Javascript</p>
            </div>
-           <div className="h-40 w-40 flex flex-col gap-2">
+           <div className="h-35 w-35 flex flex-col gap-2 lg:h-40 lg:w-40">
             <img src="https://images.icon-icons.com/171/PNG/512/html5_23329.png" alt="react img" className="h-30 object-contain p-3 rounded-lg border-gray-50 shadow-2xl"/>
             <p className="text-center font-bold text-lg">HTML</p>
            </div>
-           <div className="h-40 w-40 flex flex-col gap-2">
+           <div className="h-35 w-35 flex flex-col gap-2 lg:h-40 lg:w-40">
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/960px-CSS3_logo.svg.png" alt="react img" className="h-30 object-contain p-3 rounded-lg border-gray-50 shadow-2xl"/>
             <p className="text-center font-bold text-lg">CSS</p>
            </div>
-           <div className="h-40 w-40 flex flex-col gap-2">
+           <div className="h-35 w-35 flex flex-col gap-2 lg:h-40 lg:w-40">
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/960px-Tailwind_CSS_Logo.svg.png" alt="react img" className="h-30 object-contain p-3 rounded-lg border-gray-50 shadow-2xl"/>
             <p className="text-center font-bold text-lg">Tailwind CSS</p>
            </div>
-           <div className="h-40 w-40 flex flex-col gap-2">
+           <div className="h-35 w-35 flex flex-col gap-2 lg:h-40 lg:w-40">
             <img src="https://cdn-icons-png.flaticon.com/512/2111/2111432.png" alt="react img" className="h-30 object-contain p-3 rounded-lg border-gray-50 shadow-2xl"/>
             <p className="text-center font-bold text-lg">Git & Github</p>
            </div>
-           <div className="h-40 w-40 flex flex-col gap-2">
+           <div className="h-35 w-35 flex flex-col gap-2 lg:h-40 lg:w-40">
             <img src="https://www.svgrepo.com/show/354202/postman-icon.svg" alt="react img" className="h-30 object-contain p-3 rounded-lg border-gray-50 shadow-2xl"/>
             <p className="text-center font-bold text-lg">Postman</p>
            </div>
